99191c0d15b9610d7b2622c260662a858ea5c97b
Rifat240510 <rifat240510@gmail.com>
2025-11-01 20:56:30 +0600

    Smart-deals-Client _ file added

---
+++ src/Layouts/RootLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from '../components/Navber/Navber';
+
+
+const RootLayout = () => {
+    return (
+        <div className='max-w-7xl mx-auto'>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default RootLayout;
